00a0a521c1ec0697666a826c6cfbd0e740467f1b
yemzy <olayemioyewole@gmail.com>
2020-05-13 09:23:44 +0100

    just a little modification.

--- js/end.js
+++ js/end.js
@@ -26,5 +26,5 @@ saveHighScore = e => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("startpage.htmlccc");
+  window.location.assign("startpage.html");
 };
