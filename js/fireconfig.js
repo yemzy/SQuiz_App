505a9c532bac253feaaea1d8b9445b5a87f9f97c
kromate <akpananthony33@gmail.com>
2020-05-24 15:48:16 +0100

    login

---
+++ js/fireconfig.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCZo5tDpB5llapo--2S3jQdPZh_iofEEy4",
+    authDomain: "squijies.firebaseapp.com",
+    databaseURL: "https://squijies.firebaseio.com",
+    projectId: "squijies",
+    storageBucket: "squijies.appspot.com",
+    messagingSenderId: "898026367040",
+    appId: "1:898026367040:web:253fa422513829ff4b97a5",
+    measurementId: "G-2NZDPV5E42"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
---
+++ js/login.js
@@ -0,0 +1,24 @@
+const warning = document.getElementById('warn');
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', function(e){
+  e.preventDefault()
+  const email = document.getElementById('email').value
+  const password = document.getElementById('pass').value
+  console.log('clicked')
+
+  console.log(email)
+  console.log(password)
+
+  firebase.auth().signInWithEmailAndPassword(email, password).then(()=>{
+    alert('signed In')
+    window.location = "startpage.html"
+  }).catch(function (error) {
+    console.log(error.code, error.message)
+    warning.classList.remove("dis");
+    });
+
+  console.log('done')
+
+})
+
